7c452fa4a55ce9e494d951249ee6113e9f496585
tshegofatso <erasmustshegofatso@gmail.com>
2020-02-24 21:26:45 +0200

    added all files

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,41 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("enter text");
+            string txt = Console.ReadLine();
+
+            Console.WriteLine(prefix(txt));
+
+            Console.ReadKey();
+        }
+		static string prefix(string input)
+        {
+            if (input.Trim() == "")
+            {
+                return "0,0:";
+            }
+            else
+            {
+                int num_let = input.Length;     //num of alphabets in the string
+                string len = num_let.ToString(); // convert it to string
+
+
+                string[] words = input.Split(); //array of words
+                int num_words = words.Length; //length of words
+                string num_w = num_words.ToString();
+
+
+
+                string ans = len + "," + num_w + ":" + input; // the entire string
+
+                return ans;
+            }
+            
+
         }
     }
 }
